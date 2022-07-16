904b2e3ab1b13c84a04fb4b3a4e22889b71fdd37
keyse ahmed <77418710+keyseahmed@users.noreply.github.com>
2022-07-16 16:42:39 -0700

    Update remove.cpp

--- remove.cpp
+++ remove.cpp
@@ -10,17 +10,11 @@ string removeVowels(string s)
         if (!(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'))
         {
             s[j] = s[i];
-            //cout << " s[i] = " << s[i] << " " << endl;
-
             cout << "  " << s[j];
             j++;
         }
         i++;
     }
-
-    cout<< endl; 
-    cout << s[j-1]<< endl; 
-    cout << s.substr(0, j) << endl;
     return s.substr(0, j);
 }
 
